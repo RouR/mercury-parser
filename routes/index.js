ad560b1b454d52c5819dba687ce23f105ffe78a1
RouR <rour@yandex.ru>
2021-03-10 16:02:55 +0300

    wip web app

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const Mercury = require('@postlight/mercury-parser');
+
+/* GET home page. */
+router.get('/', function(_, res) {
+    res.send({ title: 'Welcome to Mercury Parser API', mercury: JSON.stringify(Mercury) });
+});
+
+
+router.get('/parse', async (req, res) => {
+    const result = await Mercury.parse(req.query.url);
+
+    res.send(result)
+});
+
+router.post('/parse-html', async (req, res) => {
+  const result = await Mercury.parse(
+    req.body.url,
+    { contentType: 'html', html: req.body.html });
+
+  res.send(result)
+});
+
+module.exports = router;
