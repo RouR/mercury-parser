ddac4c8873b9cb07ef73df268bfd6edb74e0e277
rour <rour@yandex.ru>
2023-02-28 18:47:22 +0300

    fix

--- routes/const.js
+++ routes/const.js
@@ -1,5 +1,5 @@
 const MyConst = {
-    userAgent: process.env.UserAgent || "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.186 Safari/537.36"
+    userAgent: process.env.UserAgent || "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 }
 
 module.exports = {
